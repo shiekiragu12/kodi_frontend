3e48f36557a4a3192922641c7143b8947abcc35a
Mugo254 <jemo496@gmail.com>
2023-03-21 15:02:27 +0300

    new commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 // Routes importing
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 // Importing Pages Auth
-import Register from './pages/auth/Register';
-import Login from './pages/auth/Login';
+import Register from "./pages/auth/Register";
+import Login from "./pages/auth/Login";
 
 // Importing main pages
-import Home from './pages/main/Home';
-
+import Home from "./pages/main/Home";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-         <Route Component={Home} path="/"/>
-          <Route Component={Login} path="/login"/>
-          <Route Component={Register} path="/register"/>
+          <Route Component={Home} exact path="/" />
+          <Route Component={Login} path="/login" />
+          <Route Component={Register} path="/register" />
         </Routes>
       </Router>
     </div>
